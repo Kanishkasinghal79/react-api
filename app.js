ef72242cf3cf5e61d9e7bba7e811458725bb94de
hemant-7802 <hsharma9667@gmail.com>
2024-05-05 00:30:48 +0530

    Deploy

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+// import packages
 import express from "express"
 import cors from "cors"
+
+// import json file using asserts
 import ProductJson from "./products.json" assert{ type: 'json'};
 
+// define a port number
 const port = 4000
+
 const app = express()
 
+// use for cors policies
 app.use(cors())
 
+// default page set 
 app.get("/", (req, res) => {
   res.send("Hello I am live...")
 })
 
+// api route 
 app.get("/api/products", (req, res) => {
+  // use json file name in send function
   res.send(ProductJson)
 })
 
